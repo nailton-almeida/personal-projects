e7ad6955d8b715c0a30e01044b5c90b2f55ac439
NailtonAlmeida <nailton.junior@mc1.com.br>
2022-09-28 11:07:55 -0300

    feature: function to convert binary in decimal

---
+++ binaryToDecimal/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class binaryToDecimal
+{
+    static int BinaryCalculator(string binary)
+    {
+        int decimalCalculated = 0;
+        int characterBackwardsRead = 1;
+
+        for (int i = 0; characterBackwardsRead <= binary.Length; i++)
+        {
+
+            int getlastBinary = ((int)Char.GetNumericValue(binary[^characterBackwardsRead]));
+            decimalCalculated += (int)(getlastBinary * (Math.Pow(2, i)));
+            characterBackwardsRead++;
+
+        }
+        return decimalCalculated;
+    }
+}
+
+
+
